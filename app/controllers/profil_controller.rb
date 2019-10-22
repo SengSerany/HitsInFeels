b89f9e6ae980f7f429e4ad57033d1ea6345b9627
SengSerany <cranyseng@gmail.com>
2019-10-22 18:02:54 +0200

    User can edit their profil

--- app/controllers/profil_controller.rb
+++ app/controllers/profil_controller.rb
@@ -3,11 +3,14 @@ class ProfilController < ApplicationController
   before_action :set_profil, only: [:show, :edit, :update]
 
   def show
+    set_age
   end
 
   def edit
   end
 
   def update
+    @profil.update(permit_profil)
+    redirect_to @profil
   end
 end
--- app/helpers/profil_helper.rb
+++ app/helpers/profil_helper.rb
@@ -1,6 +1,19 @@
 module ProfilHelper
 
     def set_profil
-        @user = User.find(params[:id])
+        @profil = Profil.find_by(user_id: params[:id])
+    end
+
+    def set_age
+        if @profil.birthday != nil
+            @profil_age = ((Time.zone.now - @profil.birthday.to_time) / 1.year.seconds).floor
+            return @profil_age
+        end
+    end
+
+    private
+
+    def permit_profil
+        params.require(:profil).permit(:first_name, :last_name, :gender, :birthday, :city)
     end
 end
